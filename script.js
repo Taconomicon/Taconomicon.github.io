b05d52de5cc7c62a2a601e1df6b101116a43194e
Dylan Mink <mink@Dylans-MacBook-Pro.local>
2018-04-12 12:31:13 -0400

    Update basic site files. Add a few interesting bits.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function party() {
   alert("It's a party!");
 }
+
+var intro = "My name is Dylan Mink. I'm learning CS at the University of Maryland, and I love to code!";
+
+function writeOut(txt, id, pos) {
+  if (pos < txt.length) {
+    document.getElementById(id).innerHTML += txt.charAt(pos);
+    setTimeout(writeOut(txt, id, pos + 1), 500);
+  }
+}
+
+writeOut(intro, "hi", 0);
